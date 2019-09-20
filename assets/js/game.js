cc8d15c9c5303cf0ee6394c072fb38c444fef22b
Paweł Bajorek <pawel.bajorek@airhelp.com>
2019-09-20 17:33:47 +0200

    logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,19 +18,19 @@ export function run_state(state) {
 function run_command(command, callback) {
   switch (command.cmd) {
     case 'move':
-      console.log('move', command, callback)
+      console.log('schedule move', command, callback)
       return ui_service.move(command, callback)
 
     case 'roll_dice':
-      console.log('roll_dice', command, callback)
+      console.log('schedule roll_dice', command, callback)
       return ui_service.roll_dice(command, callback)
 
     case 'show_card':
-      console.log('show_card', command, callback)
+      console.log('schedule show_card', command, callback)
       return ui_service.show_card(command, callback)
 
     case 'show_text':
-      console.log('show_text', command, callback)
+      console.log('schedule show_text', command, callback)
       return ui_service.show_text(command, callback)
 
     default:
