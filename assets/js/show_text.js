29c9225211ea01ebe3620785848f265ae253a8b4
Stefan Chrobot <stefan@chrobot.io>
2019-09-20 17:24:53 +0200

    Improve text

--- assets/js/show_text.js
+++ assets/js/show_text.js
@@ -3,10 +3,10 @@ import {get_command} from "../js/logic.js";
 export function cmd_show_text(state) {
   const cmd = get_command(state)
   return {
-    'start': create_show_text('Welcome', "Welcome on claim game")
+    'start': create_show_text('Welcome', "Welcome in The Claim Game!")
   }[cmd] || null
 }
 
 export function create_show_text(title, text) {
-  return {cmd: 'show_text', title, text} 
+  return {cmd: 'show_text', title, text}
 }
