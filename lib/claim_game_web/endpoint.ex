3357b3c2f8ffa5074a40b52ef59636bd4512a91e
Stefan Chrobot <stefan@chrobot.io>
2019-09-20 10:40:20 +0200

    Add static hello world page

--- lib/claim_game_web/endpoint.ex
+++ lib/claim_game_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule ClaimGameWeb.Endpoint do
     at: "/",
     from: :claim_game,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(index.html css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
