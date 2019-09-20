784ab660996c3e9608b651bd8dae3ed99a72ec63
Piotr Bzymek <piotr.bzymek@airhelp.com>
2019-09-20 16:24:10 +0200

    updated gigaelixir config

--- config/prod.secrets.exs
+++ config/prod.secrets.exs
@@ -1,5 +1,19 @@
+use Mix.Config
+
 config :claim_game, ClaimGameWeb.Endpoint,
        http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
        url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
        secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
        server: true
+
+# Configures Elixir's Logger
+config :logger, :console,
+       format: "$time $metadata[$level] $message\n",
+       metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
